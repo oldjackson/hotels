3a0a5eea2513512599f49c26cdc91fe952fc82e4
Lorenzo Fermi <Lorenzofermi@gmail.com>
2018-09-10 17:30:44 +0200

    Add first and last name to user sign up and relative validation on model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :first_name, presence: true, allow_blank: false
+  validates :last_name, presence: true, allow_blank: false
 end
