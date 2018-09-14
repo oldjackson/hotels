ed4a393a579ca0f7bca110b680bffea06ebfc64a
Lorenzo Fermi <Lorenzofermi@gmail.com>
2018-09-14 04:35:22 +0200

    Add user specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject do
+    FactoryBot.create(:user)
+  end
+  it "is valid with valid attributes" do
+    expect(subject).to be_valid()
+  end
+  it "is not valid without an email" do
+    subject.email = nil
+    expect(subject).to_not be_valid()
+  end
+  it "is not valid without a valid email" do
+    subject.email = "aaa"
+    expect(subject).to_not be_valid()
+  end
+  it "is not valid without a first name" do
+    subject.first_name = nil
+    expect(subject).to_not be_valid()
+  end
+  it "is not valid without a last name" do
+    subject.last_name = nil
+    expect(subject).to_not be_valid()
+  end
 end
